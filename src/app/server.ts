26a6cd0153dfd409f95d2fee752b35a8cbeb4a5c
tee-boy <tochukwuemmanuel690@gmail.com>
2026-07-20 17:03:49 +0100

    new changes...

---
+++ src/app/server.ts
@@ -0,0 +1,23 @@
+import app from "./app";
+import register from "./register";
+
+const start = async () => {
+  try {
+    await register(app);
+
+    const PORT = Number(process.env.PORT) || 5000;
+    const HOST = process.env.HOST || "0.0.0.0";
+
+    await app.listen({
+      port: PORT,
+      host: HOST,
+    });
+
+    console.log(`🚀 MealSync Backend running on http://localhost:${PORT}`);
+  } catch (error) {
+    app.log.error(error);
+    process.exit(1);
+  }
+};
+
+start();
