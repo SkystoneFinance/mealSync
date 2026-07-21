25f2aae67b1f44d9f8f4069de1d1f074ad7cc749
SkystoneFinance <IT@skystone-finance.com>
2026-07-21 15:12:21 +0100

    changes

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,19 @@
+// import 'dotenv/config'
+
+// const databaseUrl = process.env.DATABASE_URL
+// if (!databaseUrl) {
+//   throw new Error('DATABASE_URL is not set in .env file')
+// }
+
+// export default {
+//   datasource: {
+//     url: databaseUrl,  // Now guaranteed to be defined
+//   },
+// }
+
 import 'dotenv/config'
+import { defineConfig } from 'prisma/config'
 
-const databaseUrl = process.env.DATABASE_URL
-if (!databaseUrl) {
-  throw new Error('DATABASE_URL is not set in .env file')
-}
-
-export default {
-  datasource: {
-    url: databaseUrl,  // Now guaranteed to be defined
-  },
-}
+export default defineConfig({
+  schema: './prisma/schema.prisma',
+})
