125b370e716f30eb25d01f753c47f1d23fdc51cb
SkystoneFinance <IT@skystone-finance.com>
2026-08-06 10:34:31 +0100

    Cors policy allowed...

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -3,7 +3,10 @@ import cors from "@fastify/cors";
 
 export default fp(async (app) => {
   await app.register(cors, {
-    origin: true,
+    origin: [
+      "http://localhost:5173",
+      "https://sky-meal-sync.vercel.app",
+    ],
     credentials: true,
   });
 });
